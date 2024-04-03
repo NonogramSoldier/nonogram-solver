0cc22749edad84384a7da4ba19ba0be5679bc2c4
NonogramSoldier <94438164+NonogramSoldier@users.noreply.github.com>
2024-04-04 03:03:36 +0900

    feat: ✨ nonogram puzzleを表す構造体とその依存関係、簡単なメソッドの定義

---
+++ src/nonogram.rs
@@ -0,0 +1,45 @@
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Deserialize, Debug)]
+pub struct Puzzle {
+    initial_color: Color,
+    paint_colors: Vec<Color>,
+    clues: Clues,
+}
+
+impl Puzzle {
+    pub fn new(puzzle_name: &str) -> Self {
+        let path = &format!("puzzles/{}.json", puzzle_name);
+        let file = File::open(path).expect(&format!("Cannot open the path {}", path));
+
+        let reader = BufReader::new(file);
+
+        let puzzle: Puzzle =
+            serde_json::from_reader(reader).expect("The JSON file has an unexpected structure.");
+
+        puzzle
+    }
+
+    pub fn show(&self) {
+        println!("{:?}", self);
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Color {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Clues {
+    row: Vec<Vec<Description>>,
+    column: Vec<Vec<Description>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Description {
+    color_index: usize,
+    number: usize,
+}
