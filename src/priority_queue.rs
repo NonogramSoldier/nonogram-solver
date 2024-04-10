2e52c61110fc42daf14ab23ffc3086d3437b7180
NonogramSoldier <94438164+NonogramSoldier@users.noreply.github.com>
2024-04-10 10:21:52 +0900

    feat: ✨ heapifyの実装と、ちょっとしたリファクタリング

--- src/priority_queue.rs
+++ src/priority_queue.rs
@@ -35,7 +35,24 @@ impl<K, P, S: Default> PriorityQueue<K, P, S> {
     }
 }
 
-impl<K: Hash + Eq + Copy, P: PartialOrd, S: BuildHasher> PriorityQueue<K, P, S> {
+impl<K: Hash + Eq, P: PartialOrd, S: BuildHasher + Default> PriorityQueue<K, P, S> {
+    pub fn new_heapify(vec: Vec<(K, P)>) -> Self {
+        let mut set: HashSet<HashRef<K>, S> = HashSet::default();
+        let mut heap_vector: Vec<HeapNode<K, P>> = Vec::new();
+
+        for (key, priority) in vec {
+            let value = HashRef::new(key);
+
+            if set.insert(value.clone()) {
+                heap_vector.push(HeapNode::new(value, priority))
+            }
+        }
+
+        let heap = PriorityHeap::new_heapify(heap_vector);
+
+        Self { heap, set }
+    }
+
     pub fn insert(&mut self, key: K, priority: P) -> bool {
         let insert_value = HashRef::new(key);
 
@@ -51,7 +68,10 @@ impl<K: Hash + Eq + Copy, P: PartialOrd, S: BuildHasher> PriorityQueue<K, P, S> 
         match self.heap.pop() {
             Some(node) => {
                 self.set.remove(&node.value);
-                Some(node.value.refer.borrow().key)
+                match Rc::try_unwrap(node.value.refer) {
+                    Ok(inner) => Some(inner.into_inner().key),
+                    Err(_) => None,
+                }
             }
             None => None,
         }
@@ -82,22 +102,58 @@ impl<K, P> PriorityHeap<K, P> {
 }
 
 impl<K, P: PartialOrd> PriorityHeap<K, P> {
+    fn new_heapify(vec: Vec<HeapNode<K, P>>) -> Self {
+        let vec_len = vec.len();
+        let first_index = if vec_len == 0 { 0 } else { vec_len / 2 - 1 };
+        let mut heap = Self { heap_vector: vec };
+
+        for index in (0..first_index).rev() {
+            heap.partial_heapify(vec_len, index);
+        }
+
+        for (index, node) in heap.heap_vector.iter_mut().enumerate() {
+            node.set_index(index);
+        }
+
+        heap
+    }
+
+    fn partial_heapify(&mut self, vec_len: usize, mut index: usize) {
+        loop {
+            let mut swap_index = index;
+            let left_child = left_child(index);
+            let right_child = right_child(index);
+
+            if left_child < vec_len && self.heap_vector[swap_index] < self.heap_vector[left_child] {
+                swap_index = left_child;
+            }
+            if right_child < vec_len && self.heap_vector[swap_index] < self.heap_vector[right_child]
+            {
+                swap_index = right_child;
+            }
+
+            if swap_index == index {
+                break;
+            }
+            self.heap_vector.swap(index, swap_index);
+            index = swap_index;
+        }
+    }
+
     fn push(&mut self, value: HeapNode<K, P>) {
         self.heap_vector.push(value);
 
-        let index = self.sift_up(self.heap_vector.len() - 1);
-
-        self.heap_vector[index].set_index(index);
+        self.sift_up(self.heap_vector.len() - 1);
     }
 
     fn pop(&mut self) -> Option<HeapNode<K, P>> {
-        let heap_length = self.heap_vector.len();
+        let heap_len = self.heap_vector.len();
 
-        if heap_length <= 1 {
+        if heap_len <= 1 {
             return self.heap_vector.pop();
         }
 
-        self.heap_vector.swap(0, heap_length - 1);
+        self.heap_vector.swap(0, heap_len - 1);
         let pop_value = self.heap_vector.pop();
 
         self.sift_down(0);
@@ -135,18 +191,17 @@ impl<K, P: PartialOrd> PriorityHeap<K, P> {
     }
 
     fn sift_down(&mut self, mut index: usize) -> usize {
-        let heap_length = self.heap_vector.len();
+        let heap_len = self.heap_vector.len();
         loop {
             let mut swap_index = index;
             let left_child = left_child(index);
             let right_child = right_child(index);
 
-            if left_child < heap_length
-                && self.heap_vector[swap_index] < self.heap_vector[left_child]
+            if left_child < heap_len && self.heap_vector[swap_index] < self.heap_vector[left_child]
             {
                 swap_index = left_child;
             }
-            if right_child < heap_length
+            if right_child < heap_len
                 && self.heap_vector[swap_index] < self.heap_vector[right_child]
             {
                 swap_index = right_child;
